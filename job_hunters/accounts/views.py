8d92d9754c0465d35bfe87f167eb8c012186107e
Frillion <fridriksoebech@gmail.com>
2024-05-06 22:44:52 +0000

    fixing conflict

--- job_hunters/accounts/views.py
+++ job_hunters/accounts/views.py
@@ -6,12 +6,8 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 class LogInView(views.LoginView):
-<<<<<<< HEAD
     template_name = "account/login.html"
     next_page = "jobs/"
-=======
-    template_name = "accounts/login.html"
->>>>>>> 0b2cde8437ff0fa523692cc158e0f1e985606cae
 
     def get_user():
         return None
