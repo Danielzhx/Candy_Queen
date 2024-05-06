ad3f668d05e95ad29ce89c84ca93913179ce622a
Frillion <fridriksoebech@gmail.com>
2024-05-06 18:30:54 +0000

    [Feature]: Made the jobs page actually grab jobs from the database

--- job_hunters/jobs/views.py
+++ job_hunters/jobs/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
+from jobs.models import Job
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'jobs/index.html')
+    jobs = Job.objects.all()
+    return render(request, 'jobs/index.html',{"jobs":jobs})
 
 def detail(request, job_id):
     response = "You're looking at the results of question %s"
