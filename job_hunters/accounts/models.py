9b4562fad12506580dce14511ee0d43901299ea3
bergurh93 <bergurhalldorsson@gmail.com>
2024-05-06 23:05:46 +0000

    added account type models, other minor changes

--- job_hunters/accounts/models.py
+++ job_hunters/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Individual(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    pic = models.ImageField(default='default', upload_to='profile_images')
+    address = models.CharField(max_length=200)
+    date_of_birth = models.DateField("Date of Birth")
+    phone_number = models.CharField(max_length=20)
+
+class Company(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    pic = models.ImageField(default='default', upload_to='profile_images')
+    address = models.CharField(max_length=200)
--- job_hunters/jobs/views.py
+++ job_hunters/jobs/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from jobs.models import Job
 from django.http import HttpResponse
 
+from asgiref.sync import sync_to_async
+
 from .models import Category, Company, Job, JobType
 
 # Create your views here.
@@ -22,33 +24,25 @@ def apply(request, job_id):
 
 
 def filter_jobs(request):
-    jobs = Job.objects
-    if 'category_select' in request.POST:
+    jobs = Job.objects.all()
+    if 'category_select' in request.POST and request.POST['category_select'] != "all":
         jobs.filter(category = request.POST['category_select'])
-    if 'company_select' in request.POST:
+    if 'company_select' in request.POST and request.POST['company_select'] != "all":
         jobs.filter(company = request.POST['company_select'])
-    if 'full_time' in request.POST:
-        jobs.filter(job_type = 0)
-    if 'part_time' in request.POST:
+    if 'full_time' in request.POST and request.POST['full_time'] != 'on':
+        fulltime = get_object_or_404(JobType, job_type = "Full time")
+        jobs.filter(job_type = fulltime)
+    if 'part_time' in request.POST and request.POST['part_time'] != 'on':
         jobs.filter(job_type = 1)
-    if 'internship' in request.POST:
+    if 'internship' in request.POST and request.POST['internship'] != 'on':
         jobs.filter(job_type = 2)
+    return jobs
 
 def index2(request):
     context = {
-        "categories": Category.objects,
-        "companies": Company.objects
+        "categories": Category.objects.all(),
+        "companies": Company.objects.all(),
                }
-    jobs = Job.objects
-    if 'category_select' in request.POST:
-        jobs.filter(category = request.POST['category_select'])
-    if 'company_select' in request.POST:
-        jobs.filter(company = request.POST['company_select'])
-    if 'full_time' in request.POST:
-        jobs.filter(job_type = 0)
-    if 'part_time' in request.POST:
-        jobs.filter(job_type = 1)
-    if 'internship' in request.POST:
-        jobs.filter(job_type = 2)
-
+    
+    context['jobs'] = filter_jobs(request)
     return render(request, 'jobs/index.html', context)
