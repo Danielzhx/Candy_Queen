d3796f8e872b4aec982e7a7a7deba754b4b2b13e
bergurh93 <bergurhalldorsson@gmail.com>
2024-05-07 11:40:22 +0000

    working on signup

--- job_hunters/accounts/urls.py
+++ job_hunters/accounts/urls.py
@@ -5,9 +5,10 @@ app_name = 'accounts'
 urlpatterns = [
     path("login/", views.LogInView.as_view(), name="login"),
     path("logout/", views.log_out, name="logout"),
-    path("signup_type/", views.signup_type, name="signup_type"),
-    path("signup/", views.SignUpView.as_view(), name="signup"),
-    path("signup/new/", views.new_user, name="new_user"),
+    path("signup/", views.signup_type, name="signup_type"),
+    path("signup/user", views.register, name="register"),
+    path("signup/user/profile", views.register_profile, name="register_profile"),
+    # path("signup/company", views.c_register, name="c_register")   Implement later
     path("profile/", views.user_profile, name="profile")
     # Change password
     # Edit profile
--- job_hunters/accounts/views.py
+++ job_hunters/accounts/views.py
@@ -1,59 +1,70 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import views, authenticate, login, logout
 from django.views import generic
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
-from .models import Individual, Company
+from . import models
 
 # Create your views here.
 class LogInView(views.LoginView):
+    """View for login page. Uses built in LoginView.
+    """
     template_name = "accounts/login.html"
     next_page = "jobs:index"
 
-    def get_user():
-        return None
+    def get_user(self):
+        return self.request.user
     
 def log_out(request):
+    """Logs user out and returns them to job listing page.
+    """
     logout(request)
     return redirect("jobs:index")
     
 def signup_type(request):
-    template_name = "accounts/signup_type.html"
+    """First step in new account registration.
+    
+    Allows the user to choose whether they want to create a 
+    company or individual account.
+    """
+    template_name = "accounts/signup.html"
     return render(request, template_name, None)
 
-class SignUpView(generic.CreateView):
-    template_name = "accounts/signup.html"
-    model = User
-    fields = ["first_name","last_name","email","password"]
-
-def new_user(request):
-    if request.POST['password'] != request.POST['repeat_password']:
-        return redirect("accounts:signup")
+def register(request):
+    """Second step in new account registration for individuals.
     
-    new = User.objects.create_user(
-        username = request.POST['username'],
-        email = request.POST['email'],
-        password = request.POST['password'],
-        first_name = request.POST['first_name'],
-        last_name = request.POST['last_name']
-        )
-    ind = Individual.objects.create(
-        user = new,
-        pic = request.POST['profile_pic'],
-        address = request.POST['address'],
-        date_of_birth = request.POST['date_of_birth'],
-        phone_number = request.POST['phone']
-    )
-    new.save()
-    ind.save()
+    Takes in only the required info to create a new user. Project specific 
+    info is handled in the next step.
+    """
+    template_name = "accounts/signup_user.html"
+    if request.method == 'POST':
+        user_form = models.CustomUserForm()
+        if user_form.is_valid():
+            user_form.save()
+            return redirect('accounts:register_profile')
+        
+    else:
+        user_form = models.CustomUserForm()
+    context = {
+        'form': user_form
+        }
+    return render(request, template_name, context)
 
-    return redirect("jobs:index")
+def register_profile(request):
+    """Third step in new account registration for individuals.
+    
+    Gets project specific info which is added to both the user object and 
+    its associated individual object.
+    """
+    template_name = "accounts/signup_profile.html"
+    if request.method == 'POST':
+        profile_form = models.IndividualForm()
 
 @login_required
 def user_profile(request):
-    context = {
-        'user': request.user
-    }
+    """Allows a user to see and manage their profile."""
+    context = {'user': request.user}
     return render(request, 'accounts/profile.html', context)
