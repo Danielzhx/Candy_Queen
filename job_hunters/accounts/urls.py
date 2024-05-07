9662af2171ed345600d2b2e2cc7898dca74534ff
bergurh93 <bergurhalldorsson@gmail.com>
2024-05-07 14:11:52 +0000

    logout works, added bootstrap to base.html

--- job_hunters/accounts/urls.py
+++ job_hunters/accounts/urls.py
@@ -6,8 +6,8 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path("login/", authviews.LoginView.as_view(template_name="accounts/login.html"), name="login"),
-    path("logout/", authviews.LogoutView.as_view(next_page='accounts/login'), name="logout"),
-    path("signup/", views.signup_type, name="signup_type"),
+    path("logout/", authviews.LogoutView.as_view(next_page='accounts:login'), name="logout"),
+    path("signup/", views.signup_type, name="signup"),
     path("signup/user", views.register, name="register"),
     path("signup/user/profile", views.register_profile, name="register_profile"),
     # path("signup/company", views.c_register, name="c_register")   Implement later
