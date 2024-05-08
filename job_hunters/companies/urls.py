e45727565b37f4daa3395da8a61d7ceaeb76fd15
Maximilian Klimko <82115185+Mamigako@users.noreply.github.com>
2024-05-08 15:50:45 +0000

    companies URL added

--- job_hunters/companies/urls.py
+++ job_hunters/companies/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'companies'
 urlpatterns = [
     path("", views.index, name="index"),
+    path("companies/", views.companies, name="companies"),
     path("<int:comp_id>/", views.details, name="details"),
 ]
--- job_hunters/companies/views.py
+++ job_hunters/companies/views.py
@@ -14,3 +14,8 @@ def details(request, comp_id):
     template = "companies/profile.html"
     company = get_list_or_404(Company, pk=comp_id)
     return render(request, template, {'company': company})
+
+def companies(request):
+    template = "companies/companies.html"
+    companies = Company.objects.all()
+    return render(request, template, {'companies': companies})
