2b4e1d3b5d3829aa8a5fcea39bf6dd3ea003ce38
Maximilian Klimko <82115185+Mamigako@users.noreply.github.com>
2024-05-09 14:26:44 +0000

    formatting in classes

--- job_hunters/signup/models.py
+++ job_hunters/signup/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django import forms
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Individual(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
@@ -16,6 +17,7 @@ class IndividualForm(forms.ModelForm):
         exclude = ('user',)
         model = Individual
         fields = ['address', 'date_of_birth', 'phone_number']
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __init__(self, *args, **kwargs):
