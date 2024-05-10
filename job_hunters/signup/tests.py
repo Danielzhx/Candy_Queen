ed1ddd6cd24af9effa4227a5d43852e17be53b02
bergurh93 <bergurhalldorsson@gmail.com>
2024-05-10 17:32:52 +0000

    signup stuff

--- job_hunters/signup/tests.py
+++ job_hunters/signup/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from . import models
 
 # Create your tests here.
+
+class SignupTestCase(TestCase):
+    def create_data(self):
+        return {'user-username':'Test',
+                'user-password1':'abcdefg123',
+                'user-password2':'abcdefg123',
+                'user-first_name': 'Test',
+                'user-last_name': 'Name',
+                'individual-address': 'Address 10',
+                'individual-date_of_birth': '01-01-2000',
+                'individual-phone_number': '1234567'}
+
+    def test_successful_signup(self):
+        data = self.create_data()
+        signup = models.SignupForm(data)
+        self.assertTrue(signup.is_valid(), "Signup is valid")
