6c850565594eb1f0df574f17fcfa456c8ef7f952
Maximilian Klimko <82115185+Mamigako@users.noreply.github.com>
2024-05-10 17:14:04 +0000

    job application list link added

--- job_hunters/accounts/urls.py
+++ job_hunters/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as authviews
 
+import accounts
 from . import views
 
 app_name = 'accounts'
 urlpatterns = [
     path("profile/", views.user_profile, name="profile"),
-    path("profile/applications", views.applications, name="applications"),
+    path("profile/applications", views.view_applications, name="applications"),
 ]
