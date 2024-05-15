288a20377edf75f559912f52893b558837b5220e
bergurh93 <bergurhalldorsson@gmail.com>
2024-05-15 18:18:41 +0000

    Created custom page for create job offering page

--- job_hunters/Forms/creation_forms.py
+++ job_hunters/Forms/creation_forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm 
+from django.forms import ModelForm, widgets
 from jobs.models import Job
 from django import forms
 
@@ -7,3 +7,6 @@ class JobCreationForm(ModelForm):
     class Meta:
         model = Job
         exclude = ['company','category']
+        widgets = {
+                "job_type": widgets.Select(attrs={"placeholder":"Job type", "class":"form-select"})
+                }
